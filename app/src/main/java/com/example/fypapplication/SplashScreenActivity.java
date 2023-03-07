98ff47c161ea09db3f5aa070b4623bc77aa06f69
megan cash <C19317723@mytudublin.ie>
2023-03-07 03:26:28 +0000

    Created Splash screen.

---
+++ app/src/main/java/com/example/fypapplication/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package com.example.fypapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+        Intent intent = new Intent(SplashScreenActivity.this,LoginActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+    }
+}
